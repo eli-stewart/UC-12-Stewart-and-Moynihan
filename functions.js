de7c04126987dcadf768baa85b570652e6850544
Ryan Q Moynihan <rm73819@srcs.loc>
2017-03-01 09:30:48 -0800

    Fixed areaCode checker

--- functions.js
+++ functions.js
@@ -1,43 +1,35 @@
 /**
-* Removes part of string between two sub strings
-* @param {string} text The original string
-* @param {string} start The starting string
-* @param {string} end The ending string
-* @return {string} The string in between
-* @throws (Error} If start or end not found
-*/
+ * Removes part of string between two sub strings
+ * @param {string} text The original string
+ * @param {string} start The starting string
+ * @param {string} end The ending string
+ * @return {string} The string in between
+ * @throws (Error} If start or end not found
+ */
 function between(string, start, end) {
     var startAt = string.indexOf(start);
-
     if (startAt == -1) {
         throw new Error("No start found: " + start);
     }
-
     startAt += start.length;
     var endAt = string.indexOf(end, startAt);
-
     if (endAt == -1) {
         throw new Error("No end found: " + end);
     }
-
     return string.slice(startAt, endAt);
 }
-
 /**
  * Returns an area code from a phone number
  * @param   {string} phoneNum The phone number
  * @returns {string} The area code
  */
 function getAreaCode(phoneNum) {
-
     var areaCode = between(phoneNum, "(", ")");
-
     if (areaCode == undefined) {
         return undefined;
     }
     return areaCode;
 }
-
 /**
  * Returns an area code from a phone number: (###) ###-####
  * @param   {string} phoneNum The phone number
@@ -45,38 +37,37 @@ function getAreaCode(phoneNum) {
  * @throws {Error} If the format is incorrect
  */
 function getAreaCode(phoneNum) {
-
     var areaCode;
-
     try {
         areaCode = between(phoneNum, "(", ")");
         areaCode = areaCode.trim();
         if (areaCode.length == 3 && Number(areaCode)) {
             return areaCode;
-        } else {
+        }
+        else {
             throw new Error("Invalid area code: " + areaCode);
         }
-    } catch (error) {
+    }
+    catch (error) {
         throw new Error("Invalid phone number: " + error.message);
     }
-/**
- * Displays the area code for an inputted phone number
- * @param {string} inputId  The element id forthe text box
- * @param {string} outputId The element id of message div
- */
-
-function displayAreaCode(inputId, outputId) {
-    var outputText = "";
-    var phoneNum = document.getElementById(inputId).value;
-
-    // Now try to get the code
-    try {
-        var areaCode = getAreaCode(phoneNum);
-        outputText = "Your area code is " + areaCode;
-    } catch (error) {
-        console.log(error.message);
-        outputText = error.message;
-    }
-
-    document.getElementById(outputId).innerHTML = outputText;
 }
+    /**
+     * Displays the area code for an inputted phone number
+     * @param {string} inputId  The element id forthe text box
+     * @param {string} outputId The element id of message div
+     */
+    function displayAreaCode(inputId, outputId) {
+        var outputText = "";
+        var phoneNum = document.getElementById(inputId).value;
+        // Now try to get the code
+        try {
+            var areaCode = getAreaCode(phoneNum);
+            outputText = "Your area code is " + areaCode;
+        }
+        catch (error) {
+            console.log(error.message);
+            outputText = error.message;
+        }
+        document.getElementById(outputId).innerHTML = outputText;
+    }
