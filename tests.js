df0fa7fbe6bb8134426bb78056cfeb34b02c9564
Eli W Stewart <es04832@srcs.loc>
2017-03-01 09:16:17 -0800

    edited phoneNumberChecker() test

--- tests.js
+++ tests.js
@@ -5,13 +5,13 @@ QUnit.test("Test the getAreaCode function.", function (assert) {
     assert.deepEqual(result, "415", "Valid area code test passed.");
 
 QUnit.test("Test the phoneNumberChecker() function.", function (assert) {
-    currentInput = (415) 453-4352
-    displayCurrentInput();
-    assert.equal(document.getElementById("screen").value, "VALID", "We expect the value to be VALID.");
-    currentInput = 4153335678
-    displayCurrentInput();
-    assert.equal(document.getElementById("screen").value, "INVALID", "We expect the result to be INVALID.");
-    currentInput = 435
-    displayCurrentInput();
-    assert.equal(document.getElementById("screen").value, "INVALID", "We expect the result to be INVALID.");
+    var num = (415) 453-4352
+    phoneNumberChecker(num);
+    assert.equal(document.getElementById("output").value, "VALID", "We expect the value to be VALID.");
+    var num = 4153335678
+    phoneNumberChecker(num);
+    assert.equal(document.getElementById("output").value, "INVALID", "We expect the result to be INVALID.");
+    var num = 435
+    phoneNumberChecker(num);
+    assert.equal(document.getElementById("output").value, "INVALID", "We expect the result to be INVALID.");
 });
