a257de0677df52b6f6171df702ee259f80202da9
Ryan Q Moynihan <rm73819@srcs.loc>
2017-03-01 09:19:24 -0800

    Added end bracket

--- tests.js
+++ tests.js
@@ -3,6 +3,7 @@ QUnit.test("Test the getAreaCode() function.", function (assert) {
     var num = "(415) 555-5555";
     var result = getAreaCode(num);
     assert.deepEqual(result, "415", "Valid area code test passed.");
+}
 
 QUnit.test("Test the phoneNumberChecker() function.", function (assert) {
     var num = (415) 453-4352
