411e595549f8fca5fa13889daf8992939c8b78a1
Ryan Q Moynihan <rm73819@srcs.loc>
2017-03-09 08:12:27 -0800

    Changed tests

--- tests.js
+++ tests.js
@@ -1,15 +1,15 @@
 QUnit.test("Test the getAreaCode() function.", function (assert) {
-    var num = "(415)444-5555";
+    var num = "(415) 444-5555";
     var result = getAreaCode(num);
     assert.deepEqual(result, "415", "Valid area code test passed.");
 });
 QUnit.test("Test the getCoCode() function.", function (assert) {
-    var num = "(415)444-5555";
+    var num = "(415) 444-5555";
     var result = getCoCode(num);
     assert.deepEqual(result, "444", "Valid CO code test passed.");
 });
 QUnit.test("Test the getLineCode() function.", function (assert) {
-    var num = "(415)444-5555";
+    var num = "(415) 444-5555";
     var result = getLineCode(num);
     assert.deepEqual(result, "5555", "Valid Line code test passed.");
 });
